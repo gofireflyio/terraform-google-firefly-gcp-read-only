12d6ef7f69475b33de0616620b6c9c4bf6234f54
sefi-infralight <71797160+sefi-infralight@users.noreply.github.com>
2022-01-17 12:38:50 +0200

    firefly module

--- example/outputs.tf
+++ example/outputs.tf
@@ -1,7 +1,7 @@
 output "service_account" {
-  value = module.bridgecrew.service_account
+  value = module.firefly.service_account
 }
 
 output "project_service" {
-  value = module.bridgecrew.project_service
+  value = module.firefly.project_service
 }
