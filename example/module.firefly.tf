344d3c958c2ab786720fb065c40c0471a6fff65e
sefi-infralight <71797160+sefi-infralight@users.noreply.github.com>
2022-01-17 12:38:06 +0200

    changing project name

--- example/module.firefly.tf
+++ example/module.firefly.tf
@@ -3,5 +3,5 @@ module "firefly" {
   firefly_access_key    = "INFLXXXXXX"
   firefly_secret_key    = "XXXXXXXXX"
   name                  = "example-integration"
-  project_id            = "example-1234"
+  project_id            = "example-12345"
 }
