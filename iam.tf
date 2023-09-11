7be144c568140e9733b6c40f34d23fe1645080ac
Yuval Fatal <92246164+YuvalFireFly@users.noreply.github.com>
2023-09-11 14:48:37 +0300

    Add custom sink creation role with minimum permissions (#28)

--- iam.tf
+++ iam.tf
@@ -29,10 +29,25 @@ resource "google_project_iam_member" "service_account_project_viewer" {
   member  = "serviceAccount:${google_service_account.firefly.email}"
 }
 
+resource "google_project_iam_custom_role" "firefly_logging_custom_role" {
+  count = var.enable_event_driven ? 1 : 0
+  project = data.google_project.current.project_id
+  role_id = "firefly_logging_custom_role"
+  title = "Firefly Logging Custom Role"
+  description = "Allows Firefly to create logging sinks"
+  permissions = [
+    "logging.sinks.create",
+    "logging.sinks.delete",
+    "logging.sinks.get",
+    "logging.sinks.list",
+    "logging.sinks.update",
+  ]
+}
+
 resource "google_project_iam_member" "service_account_project_event_driven_sink_creation" {
   count  = var.enable_event_driven ? 1 : 0
   project = data.google_project.current.project_id
-  role    = "roles/logging.configWriter"
+  role    = google_project_iam_custom_role.firefly_logging_custom_role[0].name
   member  = "serviceAccount:${google_service_account.firefly.email}"
 }
 
